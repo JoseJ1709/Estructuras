2028a552f0706a8e0be252dbb9912878d59edc65
JoseJ1709 <jaramillojoseandres26@gmail.com>
2023-10-30 22:37:22 -0500

    run&arbolini1

---
+++ cmake-build-debug/CMakeFiles/Estructuras.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jose/Documentos/Estructuras/Proyecto/Estructuras/Codificacion/Arbol.cpp" "CMakeFiles/Estructuras.dir/Codificacion/Arbol.cpp.o" "gcc" "CMakeFiles/Estructuras.dir/Codificacion/Arbol.cpp.o.d"
+  "/home/jose/Documentos/Estructuras/Proyecto/Estructuras/Codificacion/Nodo.cpp" "CMakeFiles/Estructuras.dir/Codificacion/Nodo.cpp.o" "gcc" "CMakeFiles/Estructuras.dir/Codificacion/Nodo.cpp.o.d"
+  "/home/jose/Documentos/Estructuras/Proyecto/Estructuras/Estructura/continente.cpp" "CMakeFiles/Estructuras.dir/Estructura/continente.cpp.o" "gcc" "CMakeFiles/Estructuras.dir/Estructura/continente.cpp.o.d"
+  "/home/jose/Documentos/Estructuras/Proyecto/Estructuras/Estructura/jugador.cpp" "CMakeFiles/Estructuras.dir/Estructura/jugador.cpp.o" "gcc" "CMakeFiles/Estructuras.dir/Estructura/jugador.cpp.o.d"
+  "/home/jose/Documentos/Estructuras/Proyecto/Estructuras/Estructura/tarjeta.cpp" "CMakeFiles/Estructuras.dir/Estructura/tarjeta.cpp.o" "gcc" "CMakeFiles/Estructuras.dir/Estructura/tarjeta.cpp.o.d"
+  "/home/jose/Documentos/Estructuras/Proyecto/Estructuras/Estructura/territorio.cpp" "CMakeFiles/Estructuras.dir/Estructura/territorio.cpp.o" "gcc" "CMakeFiles/Estructuras.dir/Estructura/territorio.cpp.o.d"
+  "/home/jose/Documentos/Estructuras/Proyecto/Estructuras/Juego/Dado.cpp" "CMakeFiles/Estructuras.dir/Juego/Dado.cpp.o" "gcc" "CMakeFiles/Estructuras.dir/Juego/Dado.cpp.o.d"
+  "/home/jose/Documentos/Estructuras/Proyecto/Estructuras/Juego/EstadoJuego.cpp" "CMakeFiles/Estructuras.dir/Juego/EstadoJuego.cpp.o" "gcc" "CMakeFiles/Estructuras.dir/Juego/EstadoJuego.cpp.o.d"
+  "/home/jose/Documentos/Estructuras/Proyecto/Estructuras/Juego/main.cpp" "CMakeFiles/Estructuras.dir/Juego/main.cpp.o" "gcc" "CMakeFiles/Estructuras.dir/Juego/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Estructuras.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Estructuras.dir/Codificacion/Arbol.cpp.o"
+  "CMakeFiles/Estructuras.dir/Codificacion/Arbol.cpp.o.d"
+  "CMakeFiles/Estructuras.dir/Codificacion/Nodo.cpp.o"
+  "CMakeFiles/Estructuras.dir/Codificacion/Nodo.cpp.o.d"
+  "CMakeFiles/Estructuras.dir/Estructura/continente.cpp.o"
+  "CMakeFiles/Estructuras.dir/Estructura/continente.cpp.o.d"
+  "CMakeFiles/Estructuras.dir/Estructura/jugador.cpp.o"
+  "CMakeFiles/Estructuras.dir/Estructura/jugador.cpp.o.d"
+  "CMakeFiles/Estructuras.dir/Estructura/tarjeta.cpp.o"
+  "CMakeFiles/Estructuras.dir/Estructura/tarjeta.cpp.o.d"
+  "CMakeFiles/Estructuras.dir/Estructura/territorio.cpp.o"
+  "CMakeFiles/Estructuras.dir/Estructura/territorio.cpp.o.d"
+  "CMakeFiles/Estructuras.dir/Juego/Dado.cpp.o"
+  "CMakeFiles/Estructuras.dir/Juego/Dado.cpp.o.d"
+  "CMakeFiles/Estructuras.dir/Juego/EstadoJuego.cpp.o"
+  "CMakeFiles/Estructuras.dir/Juego/EstadoJuego.cpp.o.d"
+  "CMakeFiles/Estructuras.dir/Juego/main.cpp.o"
+  "CMakeFiles/Estructuras.dir/Juego/main.cpp.o.d"
+  "Estructuras"
+  "Estructuras.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Estructuras.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
